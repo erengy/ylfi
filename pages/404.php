530ea67cdffefdc5bd673e7f87661f60b47c7d51
Eren Okka <erenokka@gmail.com>
2016-10-19 10:22:29 +0300

    Remove underline tags

--- pages/404.php
+++ pages/404.php
@@ -1,5 +1,5 @@
 <hgroup class="text-center" style="padding: 20%">
     <h1>Kayıp Hata</h1>
     <h2>(Yani, böyle bir sayfa yok demek istiyoruz. Doğru geldiğinize emin misiniz?)</h2>
-    <p>Size bu sabotajı yapanı affetmeyin ve <u><a href="<?php echo $base_uri; ?>">anasayfaya</a></u> gidip düştüğü mantık hatasını yüzüne vurun.</p>
+    <p>Size bu sabotajı yapanı affetmeyin ve <a href="<?php echo $base_uri; ?>">anasayfaya</a> gidip düştüğü mantık hatasını yüzüne vurun.</p>
 </hgroup>
--- pages/faq.php
+++ pages/faq.php
@@ -1,11 +1,11 @@
         <h1 class="title">Sık Sorulan Sorular</h1>
         <dl class="faq">
             <dt><p>Sitenizde olmayan bazı mantık hataları var, farkında mısınız?</p></dt>
-            <dd><p>Evet, ancak hepsini değil önemlileri listelemek amacındayız. Fazlası için <u><a href="http://www.fallacyfiles.org/taxonomy.html" target="_blank" title="Taxonomy of the Logical Fallacies">bu detaylı listeyi</a></u> incelemenizi tavsiye ederiz.</p></dd>
+            <dd><p>Evet, ancak hepsini değil önemlileri listelemek amacındayız. Fazlası için <a href="http://www.fallacyfiles.org/taxonomy.html" target="_blank" title="Taxonomy of the Logical Fallacies">bu detaylı listeyi</a> incelemenizi tavsiye ederiz.</p></dd>
 
             <dt><p>Bu siteyi neden yaptınız ki?</p></dt>
             <dd><p>Mantık hatası ayıklamaktan keyifle tartışamaz olduk. Link verip geçmek kolayımıza geldi. Fena da olmadı hani.</p></dd>
 
             <dt><p>Siz kimsiniz?</p></dt>
-            <dd><p>Biz <u><a href="http://ozgurdusuncehareketi.org/" target="_blank" title="Özgür Düşünce Hareketi">Özgür Düşünce Hareketi</a></u>'yiz.</p></dd>
+            <dd><p>Biz <a href="http://ozgurdusuncehareketi.org/" target="_blank" title="Özgür Düşünce Hareketi">Özgür Düşünce Hareketi</a>'yiz.</p></dd>
         </dl>
